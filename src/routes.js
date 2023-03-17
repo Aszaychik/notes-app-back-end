8167fa8cbc97af1b18f18b9d6c3fe7995acddbca
Aszaychik <aszaychik@gmail.com>
2023-03-17 20:24:39 +0700

    Menampilkan Catatan

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,23 @@
-const { addNoteHandler } = require('./handler');
+/* eslint-disable max-len */
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
 
 const routes = [
-
   {
     method: 'POST',
     path: '/notes',
     handler: addNoteHandler,
   },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
 ];
+
 
 module.exports = routes;
